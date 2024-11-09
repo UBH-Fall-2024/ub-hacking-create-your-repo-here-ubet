c75ca0d2c7110049df0326400067fc82567633cf
Anthony Artega <anthony.m.arteaga@gmail.com>
2024-11-09 17:31:53 -0500

    added routing for login

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './Pages/Homepage';
+import Login from './Pages/Login';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Homepage />} />
+                
+                <Route path="/login" element={<Login />} />
                 {/* Add more routes here if needed */}
             </Routes>
         </Router>
---
+++ react-frontend/src/Pages/Login.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+function Login() {
+
+
+
+    return (
+        <div>
+            <h1>log in</h1>
+        </div>
+    );
+}
+
+export default Login;
